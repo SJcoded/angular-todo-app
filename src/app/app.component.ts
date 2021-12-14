6edf33541dfbff8dcc865edec97248afe5b5feca
SJ coded <who@sjcoded.com>
2021-12-14 16:06:46 +1100

    general todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-app';
+
+  filter: 'all' | 'active' | 'completed' = 'all';
+
+  allItems = [
+    { description: 'Buy milk', done: false },
+    { description: 'Buy bread', done: true },
+    { description: 'Buy cheese', done: false },
+  ]
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+
+    return this.allItems.filter(item => {
+      if (this.filter === 'active') {
+        return !item.done;
+      }
+
+      return item.done;
+    });
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  remove(item:Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
+
+
 }
